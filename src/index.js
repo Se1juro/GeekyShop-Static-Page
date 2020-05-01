7090646f33bfd3e8502342b4f458f68ec09db07c
Daniel Morales <mralejo99@gmail.com>
2020-05-01 15:52:32 -0500

    Proyecto actualizado Node JS

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express= require('express')
+const app = express() //Es el servidor
+const path=require('path');
+
+//Configuracion del server
+app.set('port',3000)
+app.set('views',path.join(__dirname,'vistas'))
+app.engine('html',require('ejs').renderFile)//Indico que usare la extension HTML pero EJS lo renderizara
+app.set('view engine','ejs')
+
+//Rutas
+app.use(require('./rutas/index'))
+
+//archivos estaticos
+app.use(express.static(path.join(__dirname,'publico')))//TRAIGO LOS ARCHIVOS ESTATICOS, LOS QUE USARE EN MI PAGINA
+//PARA ESO DEBO TENER LA CARPETA PUBLICO, CON LOS ARCHIVOS DENTRO
+
+app.listen(app.get('port'), ()=> { //Escuchando el server
+    console.log("Puerto de servidor: ",app.get('port'));
+});
